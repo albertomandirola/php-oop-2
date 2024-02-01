617c11f5c8741122bacd9a4b8caf55fcca3c428b
albertomandirola <albymandi00@gmail.com>
2024-02-01 09:32:32 +0100

    try adding category

--- Models/Product.php
+++ Models/Product.php
@@ -6,57 +6,61 @@ class Product{
     public $price;
     public $type;
 
-    function __construct($_name, $_brand, $_image, $_price, $_type)
+    function __construct($_name, $_brand, $_image, $_price, $_type, Category $_category)
     {
         $this->name = $_name;
         $this->brand = $_brand;
         $this->image = $_image;
         $this->price = $_price;
         $this->type = $_type;
+        $this->category = $_category;
 
     }
 
 
-    
-
 }
+
 class Cibo extends Product{
     
     public $ingredients;
     public $nutricional_value;
     public $allergens;
+    public $weight;
 
-    function __construct($_name, $_brand, $_image, $_price, $_type, $_ingredients, $_nutricional_value, $_allergens)
+    function __construct($_name, $_brand, $_image, $_price, $_type, Category $_category, $_ingredients, $_nutricional_value, $_allergens, $_weight)
     {
-        parent::__construct($_name, $_brand, $_image, $_price, $_type);
+        parent::__construct($_name, $_brand, $_image, $_price, $_type, $_category);
         $this->ingredients = $_ingredients;
         $this->nutricional_value = $_nutricional_value;
         $this->allergens = $_allergens;
+        $this->weight = $_weight;
     
     }
 }
+
 class Accessories extends Product{
     
     public $genre;
     public $utilities;
 
-    function __construct($_name, $_brand, $_image, $_price, $_type, $_genre, $_utilities )
+    function __construct($_name, $_brand, $_image, $_price, $_type, Category $_category, $_genre, $_utilities )
     {
-        parent::__construct($_name, $_brand, $_image, $_price, $_type);
+        parent::__construct($_name, $_brand, $_image, $_price, $_type, $_category);
         $this->genre = $_genre;
         $this->utilities = $_utilities;
         
     
     }
 }
+
 class Game extends Product{
 
     public $genre;
     public $scope;
 
-    function __construct($_name, $_brand, $_image, $_price, $_type, $_genre, $_scope )
+    function __construct($_name, $_brand, $_image, $_price, $_type, Category $_category, $_genre, $_scope )
     {
-        parent::__construct($_name, $_brand, $_image, $_price, $_type);
+        parent::__construct($_name, $_brand, $_image, $_price, $_type, $_category);
         $this->genre = $_genre;
         $this->scope = $_scope;
         
--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 require __DIR__.'/Models/Product.php';
 require __DIR__.'/Models/Category.php';
 
-$product_1 = new Cibo("light-food", "acana", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/ACANA-reg-dog-pacifica-fr-xl-600x600.jpg", 25.90, "sterilizzato", ["pollo","verdura","omega-3","carni miste"], [15,100,23,45], ["tapioca","glutine"]);
-$product_2 = new Cibo("mass-food", "whiskas", "https://www.cicalia.com/it/img/imgproducts/64080/m_64080.jpg", 35, "tutti", ["tonno","verdura","omega-3","scarti pesce"], [14,111,23,5], "acciuga");
+$dog = new Category("Dog");
+$cat = new Category("Cat");
+$bird = new Category("Bird");
+$fish = new Category("FIsh");
+
+$product_1 = new Cibo("light-food", "acana", "https://www.moby-dick.it/image/cache/data/Foto%20Prodotti/Cani/ACANA-reg-dog-pacifica-fr-xl-600x600.jpg", 25.90, "sterilizzato",$dog , ["pollo","verdura","omega-3","carni miste"], [15,100,23,45], ["tapioca","glutine"], 10);
+$product_2 = new Cibo("mass-food", "whiskas", "https://www.cicalia.com/it/img/imgproducts/64080/m_64080.jpg", 35, "tutti",$cat, ["tonno","verdura","omega-3","scarti pesce"], [14,111,23,5], "acciuga", 1.5);
 
 
 
